3864b581001da8a81be51a0cef4bb6ac4373612f
Lotus1337 <75097219+Lotus1337@users.noreply.github.com>
2020-11-28 10:53:58 -0600

    Updated Calculator with 2 new functions
    New functions are being able to cube numbers and being able to multiply by pi. (pi is shortened to 3.14)

--- Calculator.py
+++ Calculator.py
@@ -28,6 +28,10 @@ def cube(x):
 def reci(x):
     return 1 / x
 
+#multiply by pi
+def pi(x):
+    return x * 3.14
+
 #input (selecting operation)
 print("Select operation")
 print("1. Add")
@@ -37,14 +41,15 @@ print("4. Divide")
 print("5. Square")
 print("6. Cube")
 print("7. Reciprocal")
+print("8. Multiply by pi")
 
 #output after num1 and num2 are entered
 while True:
-    choice = input("Enter choice(1/2/3/4/5/6/7)")
+    choice = input("Enter choice(1/2/3/4/5/6/7/8)")
 
-    if choice in ('1', '2', '3', '4', '5', '6', '7'):
+    if choice in ('1', '2', '3', '4', '5', '6', '7', '8'):
         num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number (If choice is 5, 6, or 7, just enter 1): "))
+        num2 = float(input("Enter second number (If choice is 5, 6, 7, or 8 just enter 1): "))
 
         if choice == '1':
             print(num1, "+", num2, "=", add(num1, num2))
@@ -66,5 +71,8 @@ while True:
 
         elif choice == '7':
             print("1", "/", num1, "=", reci(num1))
+
+        elif choice == '8':
+            print(num1, "x", "pi", "= about", pi(num1))
     else:
         print("Invalid Input")
